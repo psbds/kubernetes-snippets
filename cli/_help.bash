eaab97ae31c66380975925f318dd5a92b67ec4d6
Paulo Baima <padasil@microsoft.com>
2020-04-04 03:46:03 -0300

    Adding help for default cli command

---
+++ cli/_help.bash
@@ -0,0 +1,15 @@
+set -e
+
+usage(){
+    echo "
+Kubernetes Snippets
+
+See more at: $(printInfo https://github.com/psbds/kubernetes-snippets)
+
+Commands: 
+
+    cluster                     Kubernetes Snippets for Cluster Management.
+    devops                      DevOps utilities for Kubernetes.
+"
+    
+}
